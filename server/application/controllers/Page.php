17b1bef24f96604952920579c5459f3f6232baa1
bedisdover <sym14@software.nju.edu.cn>
2016-11-16 09:59:24 +0800

    docs

---
+++ server/application/controllers/Page.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: song
+ * Date: 16-11-5
+ * Time: 下午5:30
+ */
+class Page extends CI_Controller
+{
+
+    public function test()
+    {
+        echo "test";
+    }
+}
